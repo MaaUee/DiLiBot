a704514caae9d1a8c2445566252c38505f3f6443
manuel.serra <manuel.serra@internetstores.com>
2018-09-26 13:53:18 +0200

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../dilibot.zip');
+var kuduApi = 'https://dilibot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$dilibot';
+var password = 'nCJf7fiBeHxle7w0H642mFxfP1ZKEifEAozCE5np94cHWjzq1XL9ssaP74om';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('dilibot publish');
+  } else {
+    console.error('failed to publish dilibot', err);
+  }
+});
